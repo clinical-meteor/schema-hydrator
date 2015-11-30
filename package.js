f52df2b1c5403fa3ea6a1b80789d5d3fcff4c35a
Abigail Watson <awatson1978@gmail.com>
2015-11-30 13:59:32 -0800

    updated package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:schema-hydrator',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Hydrates/dehydrates a SimpleSchema into something that can be stored in Mongo.',
   git: 'http://github.com/clinical-meteor/clinical-schema-hydrator',
   documentation: 'README.md'
@@ -8,11 +8,16 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
+
+  api.use('aldeed:simple-schema@1.1.0');
+
   api.addFiles('lib/SchemaHydrator.js');
+
+  api.export('SchemaHydrator', 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('clinical:schema-hydrator');
-  api.addFiles('schema-hydrator-tests.js');
+  api.addFiles('tests/schema-hydrator-tests.js');
 });
