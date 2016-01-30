53c44032c7bd9d37aaa3d5dba542cf2ed0b745c9
Abigail Watson <awatson1978@gmail.com>
2016-01-29 20:00:45 -0800

    removed tinytests; added simple-schema dep

--- tests/gagarin/SchemaHydratorTests.js
+++ tests/gagarin/SchemaHydratorTests.js
@@ -3,19 +3,6 @@ describe('clinical:schema-hydrator', function () {
   var server = meteor();
   var client = browser(server);
 
-  beforeEach(function () {
-    server.execute(function () {
-
-    }).then(function (value){
-
-    });
-  });
-  afterEach(function () {
-    server.execute(function () {
-
-    });
-  });
-
   it('SchemaHydrator should exist on the client', function () {
     return client.execute(function () {
       expect(SchemaHydrator).to.exist;
