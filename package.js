44d53fe7acaa5feb30a3c05298eb92f94f3e5002
Abigail Watson <awatson1978@gmail.com>
2016-12-03 21:03:56 -0600

    circle.yml

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:schema-hydrator',
-  version: '0.1.1',
+  version: '0.3.0',
   summary: 'Hydrates/dehydrates a SimpleSchema into something that can be stored in Mongo.',
   git: 'http://github.com/clinical-meteor/clinical-schema-hydrator',
   documentation: 'README.md'
@@ -9,15 +9,9 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use('aldeed:simple-schema@1.1.0');
+  api.use('aldeed:simple-schema@1.3.3');
 
   api.addFiles('lib/SchemaHydrator.js');
 
   api.export('SchemaHydrator');
-});
-
-Package.onTest(function (api) {
-  api.use('tinytest');
-  api.use('clinical:schema-hydrator');
-  api.addFiles('tests/schema-hydrator-tests.js');
 });
